05fc5179b6b94fe58d8baea409cee8096060e6f6
Chaeb24 <2003dolmen@naver.com>
2025-12-29 17:18:09 +0900

    친구 1028번 고침

--- hw_08_251229.py
+++ hw_08_251229.py
@@ -1,23 +1,23 @@
-T = int(input()) # 테스트 케이스 개수
+n = int(input())
+friends = [input().strip() for _ in range(n)]
 
-queue = []
-visited = [] # 방문 여부 체크
-for _ in range(T):
-    data = input().split()  # 몇 개가 오든 리스트로 저장
-    queue.append(data)
+answer = 0
 
-def bfs(queue,visited):
-    count = 0 # 친구 몇 명인지 세기
-    for _ in range(1,3): # 2번째 친구까지만 확인
-        a = queue.pop()
-        visited.append(a)
-        visited[a] = True
-        count += 1
-        if not visited[a]:
-            bfs(queue, visited)
-    print(count)
+for i in range(n):
+    visited = [False] * n
+    cnt = 0
+    for j in range(n):
+        if i == j:
+            continue
+        if friends[i][j] == 'Y':  # 직접 친구
+            visited[j] = True
+        else:
+            # 친구의 친구 체크
+            for k in range(n):
+                if friends[i][k] == 'Y' and friends[k][j] == 'Y':
+                    visited[j] = True
+                    break
+    cnt = sum(visited)
+    answer = max(answer, cnt)
 
-bfs(queue, visited)
-
-
-
+print(answer)
